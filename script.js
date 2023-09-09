735ccf11c180efcafe8ff6eac9ae5be12ac6340d
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-09-09 15:37:35 +0100

    rendered products dynamically js and displayed products

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
-const clearCartBtn = document.querySelector(".clear-bart");
+const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
@@ -30,8 +30,34 @@ class Products {
     }
   }
 }
-// display products
-class UI {}
+ //display products
+class UI {
+displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+           <!-- single product -->  
+        <article class="product">
+          <div class="image-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>£${product.price}</h4>
+        </article>
+        <!-- end of single product --> 
+      `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 //local storage
 class storage {}
 document.addEventListener("DOMContentLoaded", () => {
@@ -39,5 +65,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   //get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
